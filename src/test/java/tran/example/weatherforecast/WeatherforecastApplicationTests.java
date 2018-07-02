a4ec47e7f10e368097389912c942fde1e6b59a41
todd <toddtran9@gmail.com>
2018-07-02 14:14:10 -0700

    Adding in a comments.

--- src/test/java/tran/example/weatherforecast/WeatherforecastApplicationTests.java
+++ src/test/java/tran/example/weatherforecast/WeatherforecastApplicationTests.java
@@ -5,6 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * This is a default test...
+ * I will likely remove this later on as it is only checking if the application context can be
+ * loaded in.
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WeatherforecastApplicationTests {
