011026d1763c6b994488193c65f2c6d19ab488ca
todd <toddtran9@gmail.com>
2018-07-02 15:33:28 -0700

    Interface to declare expected CRUD methods and behaviors for subclasses using Role objects.

---
+++ src/main/java/tran/example/weatherforecast/services/RoleService.java
@@ -0,0 +1,9 @@
+package tran.example.weatherforecast.services;
+
+import tran.example.weatherforecast.domain.security.Role;
+
+/**
+ * Interface for declaring CRUD methods for Role objects.
+ */
+public interface RoleService extends CRUDService<Role> {
+}
