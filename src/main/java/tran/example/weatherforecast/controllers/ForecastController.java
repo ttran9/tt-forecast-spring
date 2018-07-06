9057e43cd465a93924f3843f335cbbb7a07d9baf
todd <toddtran9@gmail.com>
2018-07-06 02:34:10 -0700

    Adding a model onto each of the view mappings and using inheritance to assign a title attribute.

--- src/main/java/tran/example/weatherforecast/controllers/ForecastController.java
+++ src/main/java/tran/example/weatherforecast/controllers/ForecastController.java
@@ -2,6 +2,7 @@ package tran.example.weatherforecast.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 @RequestMapping({ForecastController.BASE_URL})
-public class ForecastController {
-
+public class ForecastController extends BaseController {
     /**
      * The name of view that displays the prior made by a user!
      */
@@ -24,14 +24,20 @@ public class ForecastController {
      * The name of the directory containing all the views for requesting pages related to forecasts.
      */
     public static final String BASE_VIEW_URL_RETURN = "forecast";
+    /**
+     * The title of the page that displays a user's prior searches.
+     */
+    public static final String PRIOR_USER_SEARCHES_TITLE = "Prior Searches";
 
     /**
      * Processes a request to view the user's prior searches.
+     * @param model UI component holding attributes to be used in the view.
      * @return The name of view prior searches page.
      */
     @RequestMapping(IndexController.URL_PATH_SEPARATOR + USER_SEARCH_VIEW_NAME)
-    public String getSearchesPage() {
+    public String getSearchesPage(Model model) {
         log.debug("Showing a user's prior searches!");
+        addTitleAttribute(model, PRIOR_USER_SEARCHES_TITLE);
         return BASE_VIEW_URL_RETURN + IndexController.URL_PATH_SEPARATOR + USER_SEARCH_VIEW_NAME;
     }
 }
