391d339bcfd4a235b8173d8305c1bd1955cd846e
todd <toddtran9@gmail.com>
2018-07-09 14:47:14 -0700

    Domain objects used to assist with the mapping of the JSON data to Java objects from the Google Geocoding API.

---
+++ src/main/java/tran/example/weatherforecast/domain/geocode/Result.java
@@ -0,0 +1,20 @@
+package tran.example.weatherforecast.domain.geocode;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * The data members of this class hold information about the address entered such as the entered
+ * address and the latitude and longitude (location) for this address.
+ */
+@Getter
+@Setter
+@JsonIgnoreProperties(value = {"address_components", "place_id", "types"})
+public class Result {
+    @JsonProperty(value = "formatted_address")
+    private String formattedAddress;
+    @JsonProperty(value = "geometry")
+    private Geometry geometry;
+}
