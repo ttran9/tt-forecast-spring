760377990d293b8ed4b598731840eca3c6cb9d3d
todd <toddtran9@gmail.com>
2018-07-10 02:41:08 -0700

    Service which allows for saving of searches and obtaining searches made by a user.

---
+++ src/main/java/tran/example/weatherforecast/services/forecastservices/SearchService.java
@@ -0,0 +1,28 @@
+package tran.example.weatherforecast.services.forecastservices;
+
+import tran.example.weatherforecast.domain.Search;
+
+import java.util.List;
+
+/**
+ * Declares methods to perform read and insertion operations for Search object(s).
+ */
+public interface SearchService {
+
+    /**
+     * Gets a list of searches made by a user based on the id of the user provided.
+     * @param userId The id value to identify a user.
+     * @return Returns a list of Search objects.
+     */
+    List<Search> getSearchesByUserId(Long userId);
+
+    /**
+     * Saves the search into the database.
+     * Note: The search will have the daily and hourly forecasts in it allowing those to be
+     * persisted when the search is saved.
+     * @param search The search object to be persisted.
+     * @param userId The id value to identify a user.
+     * @return Returns the object that was saved into the database.
+     */
+    Search saveSearch(Search search, Long userId);
+}
---
+++ src/main/java/tran/example/weatherforecast/services/forecastservices/SearchServiceImpl.java
@@ -0,0 +1,94 @@
+package tran.example.weatherforecast.services.forecastservices;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tran.example.weatherforecast.domain.Search;
+import tran.example.weatherforecast.domain.User;
+import tran.example.weatherforecast.exceptions.NotFoundException;
+import tran.example.weatherforecast.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Implementation of methods to save a Search made by a specified User and to find all the
+ * searches made by a user.
+ */
+@Service
+@Slf4j
+public class SearchServiceImpl implements SearchService {
+
+    /**
+     * Allows access to retrieve the User and to update the associated user object.
+     */
+    private UserRepository userRepository;
+
+    /**
+     * Performs DI and wires the userRepository object accordingly so CRUD operations can be
+     * performed.
+     * @param userRepository The dependency required to wire this service to perform CRUD
+     *                       operations.
+     */
+    @Autowired
+    public SearchServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Finds a user based on the specified id and returns a list of the searches made by the user.
+     * @param userId The id value to identify a user.
+     * @return Returns a list of searches made by a user.
+     */
+    @Override
+    public List<Search> getSearchesByUserId(Long userId) {
+        String debugMessage = "attempting to find the searches made by a user with id: " + userId;
+        String exceptionMessage = "The user could not be found while attempting to retrieve" +
+                " the searches";
+        User user = checkIfUserIsPresent(debugMessage, exceptionMessage, userId);
+        return user.getSearches();
+    }
+
+    /**
+     * Adds the search to a list of searches made by the user and then updates this user object
+     * which saves the search into the database.
+     * @param search The search object to be persisted.
+     * @param userId The id value to identify a user.
+     * @return Returns the search object saved to the database.
+     */
+    @Override
+    public Search saveSearch(Search search, Long userId) {
+        String debugMessage = "attempting to save the search made by user with" +  "with id: " +
+                userId;
+        String exceptionMessage = "The user could not be found while attempting to retrieve" +
+                " the searches";
+        User user = checkIfUserIsPresent(debugMessage, exceptionMessage, userId);
+        // do other things to save the search.
+        user.addSearch(search);
+        User userWithUpdatedSearches = userRepository.save(user);
+        /**
+         * returning the last element in the searches list because I assume the user cannot save
+         * the same search twice and if this was possible it would have identical content except
+         * the ID would differ which would not make a difference to the user viewing forecasts.
+         */
+        return userWithUpdatedSearches.getSearches().get(userWithUpdatedSearches.getSearches()
+                .size() - 1);
+    }
+
+    /**
+     * helper method to check if the user can be found from the specified user id..
+     * @param debugMessage The debug message to print to the console.
+     * @param notFoundExceptionMessage The message that indicates there was an error finding the
+     *                                 user if applicable
+     * @param userId The id of the logged in user.
+     */
+    private User checkIfUserIsPresent(String debugMessage, String notFoundExceptionMessage, Long
+            userId) {
+        log.debug(debugMessage);
+        Optional<User> userOptional = userRepository.findById(userId);
+        if(!userOptional.isPresent()) {
+            throw new NotFoundException(notFoundExceptionMessage);
+        }
+        return userOptional.get();
+    }
+}
