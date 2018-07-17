dcfe10228fc2620eb20c633409eb5b7814f1858d
todd <toddtran9@gmail.com>
2018-07-17 16:35:01 -0700

    Tests if the user can view the registration page.

---
+++ src/test/java/tran/example/weatherforecast/controllers/RegistrationControllerTest.java
@@ -0,0 +1,83 @@
+package tran.example.weatherforecast.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.Validator;
+import tran.example.weatherforecast.commands.RegistrationFormCommand;
+import tran.example.weatherforecast.domain.User;
+import tran.example.weatherforecast.repositories.RoleRepository;
+import tran.example.weatherforecast.repositories.UserRepository;
+import tran.example.weatherforecast.services.registrationservices.RegistrationService;
+import tran.example.weatherforecast.services.registrationservices.RegistrationServiceImpl;
+import tran.example.weatherforecast.services.security.EncryptionService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * This class will test if the register page can be shown to the user.
+ */
+public class RegistrationControllerTest {
+    /**
+     * Entry point for MVC testing.
+     */
+    private MockMvc mockMvc;
+    /**
+     * Used to save the user to the User table.
+     */
+    @Mock
+    private UserRepository userRepository;
+    /**
+     * Used to get the role object with the role "User"
+     */
+    @Mock
+    private RoleRepository roleRepository;
+    /**
+     * Used to assign the user name and password from the registration form into a User object's
+     * username and password fields.
+     */
+    @Mock
+    private Converter<RegistrationFormCommand, User> converter;
+    /**
+     * Used to encrypt the password.
+     */
+    @Mock
+    private EncryptionService encryptionService;
+    /**
+     * Custom validator used for password verification.
+     */
+    @Mock
+    private Validator validator;
+
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        RegistrationService registrationService = new RegistrationServiceImpl(userRepository,
+                roleRepository, converter, encryptionService);
+        RegistrationController registerController = new RegistrationController
+                (registrationService, validator);
+        mockMvc = MockMvcBuilders.standaloneSetup(registerController).build();
+    }
+
+    /**
+     * Tests if the user can make the request to the registration page and if there is a title
+     * attribute.
+     * @throws Exception Throws an exception of the mock HTTP get request cannot be made.
+     */
+    @Test
+    public void getRegistrationPage() throws Exception {
+        mockMvc.perform(get(RegistrationController.BASE_URL))
+                .andExpect(view().name(RegistrationController.REGISTRATION_DIRECTORY
+                        + RegistrationController.REGISTRATION_PAGE_NAME))
+                .andExpect(model().attribute(IndexController.PAGE_ATTRIBUTE,
+                        RegistrationController.REGISTRATION_VIEW_TITLE))
+                .andExpect(status().isOk());
+    }
+
+}
