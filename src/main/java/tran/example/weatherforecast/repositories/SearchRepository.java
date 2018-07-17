4c4d57178db94221b26de17df2be8a2a68f629b5
todd <toddtran9@gmail.com>
2018-07-17 16:12:39 -0700

    Adding in a method declaration to get all the searches made by a user.

--- src/main/java/tran/example/weatherforecast/repositories/SearchRepository.java
+++ src/main/java/tran/example/weatherforecast/repositories/SearchRepository.java
@@ -1,7 +1,19 @@
 package tran.example.weatherforecast.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import tran.example.weatherforecast.domain.Search;
 
+/**
+ * This interface will allow access to get a list of paginated searches from the Search table.
+ */
 public interface SearchRepository extends PagingAndSortingRepository<Search, Long> {
+    /**
+     * Finds searches by a specific user.
+     * @param pageable An object containing information to paginate the results.
+     * @param id The id of the user the search is associated with.
+     * @return Returns a paginated sublist by the user specified by the id.
+     */
+    Page<Search> findAllByUserId(Pageable pageable, Long id);
 }
