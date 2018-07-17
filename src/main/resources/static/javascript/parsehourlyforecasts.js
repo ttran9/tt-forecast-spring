77fa815badf57b9e1bb9f474d68d76aef358705a
todd <toddtran9@gmail.com>
2018-07-17 16:28:50 -0700

    Methods used to generate forecasts onto graphs leveraging CanvasJS.

---
+++ src/main/resources/static/javascript/parsehourlyforecasts.js
@@ -0,0 +1,38 @@
+/*
+ * generates the column graph with the hourly forecasts.
+ */
+function generateHourlyForecast(hourlyList) {
+    var title = "Hourly Forecast";
+    var yAxisLabel = "Temperature";
+    var xAxisLabel = "Time And Date";
+    var chartId = "hourlyForecastChart";
+
+    var hourlyForecasts = [];
+    for (var i = 0; i < hourlyList.length; i++) {
+        var hourlyForecast = hourlyList[i];
+        hourlyForecasts.push({
+            label : hourlyForecast.label,
+            y : hourlyForecast.y
+        });
+    }
+
+    var chart = new CanvasJS.Chart(chartId, {
+        theme: "light2", // "light1", "dark1", "dark2"
+        animationEnabled: true,
+        title: {
+            text: title
+        },
+        axisY: {
+            title: yAxisLabel
+        },
+        axisX: {
+            title: xAxisLabel
+        },
+        data: [{
+            type: "column",
+            dataPoints: hourlyForecasts
+        }]
+    });
+
+    chart.render();
+}
