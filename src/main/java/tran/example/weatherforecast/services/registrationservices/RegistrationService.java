1895f18c59250e421794597d6d374aba22688127
todd <toddtran9@gmail.com>
2018-07-17 16:27:56 -0700

    Declares a method which encrypts the plain text user password and attempts to create/save the user.

---
+++ src/main/java/tran/example/weatherforecast/services/registrationservices/RegistrationService.java
@@ -0,0 +1,18 @@
+package tran.example.weatherforecast.services.registrationservices;
+
+import tran.example.weatherforecast.commands.RegistrationFormCommand;
+import tran.example.weatherforecast.domain.User;
+
+/**
+ * Declares a method to process user registration (create a user).
+ */
+public interface RegistrationService {
+
+    /**
+     * Takes in the userName and password from the form and saves it into the User object and
+     * then the password is encrypted and the user is saved into the database.
+     * @param registrationFormCommand The object holding the user name and password.
+     * @return Returns a user object with a username and an encrypted password.
+     */
+    User registerUser(RegistrationFormCommand registrationFormCommand);
+}
