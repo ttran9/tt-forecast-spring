034e5404fff2b2aa12d2a22f7ae9ad35dd3e1b56
todd <toddtran9@gmail.com>
2018-07-17 16:08:54 -0700

    This change leads to the IDs of objects being in sequence versus being values being more spread apart.

--- src/main/java/tran/example/weatherforecast/domain/AbstractDomainClass.java
+++ src/main/java/tran/example/weatherforecast/domain/AbstractDomainClass.java
@@ -18,17 +18,17 @@ public class AbstractDomainClass implements DomainObject {
      * The unique identifier for objects of a table.
      */
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     /**
      * A field to store when an object was first persisted into the database.
      */
-    private Date dateCreated;
+    protected Date dateCreated;
     /**
      * A field to store when an object was last modified or persisted into the database.
      */
-    private Date lastUpdated;
+    protected Date lastUpdated;
 
     /**
      * A method to be invoked before an object is saved/persisted or updated.
