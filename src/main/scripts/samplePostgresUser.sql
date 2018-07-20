a40cfc9237e99fe3f437466df7e993e56fd33bc0
todd <toddtran9@gmail.com>
2018-07-19 19:15:16 -0700

    Adding necessary permissions.

--- src/main/scripts/samplePostgresUser.sql
+++ src/main/scripts/samplePostgresUser.sql
@@ -9,3 +9,10 @@ ALTER DEFAULT PRIVILEGES in SCHEMA public GRANT SELECT ON TABLES TO sample_user;
 ALTER DEFAULT PRIVILEGES in SCHEMA public GRANT DELETE ON TABLES TO sample_user;
 ALTER DEFAULT PRIVILEGES in SCHEMA public GRANT UPDATE ON TABLES TO sample_user;
 ALTER DEFAULT PRIVILEGES in SCHEMA public GRANT INSERT ON TABLES TO sample_user;
+
+GRANT USAGE, SELECT ON SEQUENCE search_id_seq TO sample_user;
+GRANT USAGE, SELECT ON SEQUENCE daily_forecast_id_seq TO sample_user;
+GRANT USAGE, SELECT ON SEQUENCE hourly_forecast_id_seq TO sample_user;
+GRANT USAGE, SELECT ON SEQUENCE custom_user_id_seq TO sample_user;
+GRANT USAGE, SELECT ON SEQUENCE role_id_seq TO sample_user;
+GRANT USAGE, SELECT ON SEQUENCE search_id_seq TO sample_user;
