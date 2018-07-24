e5bb9cffd5401ae05ba0a05b57e4722ffcbe6f2b
todd <toddtran9@gmail.com>
2018-07-24 13:57:46 -0700

    Adding in comments and swapping in when the user is created.

--- src/main/scripts/samplePostgresUser.sql
+++ src/main/scripts/samplePostgresUser.sql
@@ -1,15 +1,16 @@
--- This will prevent default users from creating tables
-REVOKE CREATE ON SCHEMA public FROM public;
-
--- create the user
+-- create the user (step 1)
 CREATE USER sample_user WITH PASSWORD 'testpw';
 
--- Grant the user the necessary permissions.
+-- This will prevent default users from creating tables (step 1)
+REVOKE CREATE ON SCHEMA public FROM public;
+
+-- Grant the user the necessary permissions. (step 1)
 ALTER DEFAULT PRIVILEGES in SCHEMA public GRANT SELECT ON TABLES TO sample_user;
 ALTER DEFAULT PRIVILEGES in SCHEMA public GRANT DELETE ON TABLES TO sample_user;
 ALTER DEFAULT PRIVILEGES in SCHEMA public GRANT UPDATE ON TABLES TO sample_user;
 ALTER DEFAULT PRIVILEGES in SCHEMA public GRANT INSERT ON TABLES TO sample_user;
 
+-- after tables have been generated this should be run. (step 3)
 GRANT USAGE, SELECT ON SEQUENCE search_id_seq TO sample_user;
 GRANT USAGE, SELECT ON SEQUENCE daily_forecast_id_seq TO sample_user;
 GRANT USAGE, SELECT ON SEQUENCE hourly_forecast_id_seq TO sample_user;
