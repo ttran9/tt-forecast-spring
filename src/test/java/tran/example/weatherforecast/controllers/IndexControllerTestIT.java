2c1450d5ef75c50d0f121a4cdda5fd6a064bbdb1
todd <toddtran9@gmail.com>
2018-07-25 14:12:27 -0700

    Refactoring tests to check for test coverage.

--- src/test/java/tran/example/weatherforecast/controllers/IndexControllerTestIT.java
+++ src/test/java/tran/example/weatherforecast/controllers/IndexControllerTestIT.java
@@ -19,8 +19,9 @@ import java.util.LinkedList;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -55,5 +56,55 @@ public class IndexControllerTestIT {
                 IndexController.LOGIN_PAGE_MAPPING))
                 .andExpect(forwardedUrl(IndexController.DENIED_PAGE_MAPPING))
                 .andExpect(status().is4xxClientError());
+    }
+
+    /**
+     * Tests if the user can go to the index page with the http status code of 200 (ok).
+     * @throws Exception If there is an error performing the get request.
+     */
+    @Test
+    public void getIndexPage() throws Exception {
+        mockMvc.perform(get(IndexController.URL_PATH_SEPARATOR))
+                .andExpect(view().name(IndexController.INDEX_VIEW_NAME))
+                .andExpect(model().attribute(ControllerHelper.PAGE_ATTRIBUTE, IndexController.INDEX_PAGE_TITLE))
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * Tests if the user can view the denied page which is open to all users.
+     * @throws Exception If there is an error performing the get request.
+     */
+    @Test
+    public void getAccessDeniedPage() throws Exception {
+        mockMvc.perform(get(IndexController.DENIED_PAGE_MAPPING))
+                .andExpect(view().name(IndexController.DENIED_VIEW_NAME))
+                .andExpect(model().attribute(ControllerHelper.PAGE_ATTRIBUTE, IndexController.ACCESS_DENIED_PAGE_TITLE))
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * Tests if the user can view the login/signin page which is open to all users.
+     * @throws Exception If there is an error performing the get request.
+     */
+    @Test
+    public void getLoginForm() throws Exception {
+        mockMvc.perform(get(IndexController.URL_PATH_SEPARATOR +
+                IndexController.LOGIN_PAGE_MAPPING))
+                .andExpect(status().isOk())
+                .andExpect(view().name(IndexController.SIGNIN_VIEW_NAME))
+                .andExpect(model().attribute(ControllerHelper.PAGE_ATTRIBUTE, IndexController.LOGIN_PAGE_TITLE));
+    }
+
+    /**
+     * Tests if the user can view the resource not found page which is open to all users.
+     * @throws Exception If there is an error performing the get request.
+     */
+    @Test
+    public void getNotFoundPage() throws Exception {
+        mockMvc.perform(get(IndexController.URL_PATH_SEPARATOR +
+                IndexController.NOT_FOUND_MAPPING))
+                .andExpect(status().isOk())
+                .andExpect(view().name(IndexController.NOT_FOUND_VIEW_NAME))
+                .andExpect(model().attribute(ControllerHelper.PAGE_ATTRIBUTE, IndexController.RESOURCE_NOT_FOUND_PAGE_TITLE));
     }
 }
