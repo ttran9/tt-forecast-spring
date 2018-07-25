ffae5063b12db173e64b1ed6b80fdb18b4952db8
todd <toddtran9@gmail.com>
2018-07-25 15:27:46 -0700

    Removing unused methods.

--- src/main/java/tran/example/weatherforecast/exceptions/NotFoundException.java
+++ src/main/java/tran/example/weatherforecast/exceptions/NotFoundException.java
@@ -5,24 +5,10 @@ package tran.example.weatherforecast.exceptions;
  * application and caught by the proper exception handler helper methods.
  */
 public class NotFoundException extends RuntimeException  {
-
-    /**
-     * Constructs a new notfound exception without a message.
-     */
-    public NotFoundException() {
-    }
-
     /**
      * Constructs a new notfound exception with a message.
      */
     public NotFoundException(String message) {
         super(message);
-    }
-
-    /**
-     * Constructs a new notfound exception with a message and cause
-     */
-    public NotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
