bb9685f2e11824528b1f11e9f39202341b3c18a6
todd <toddtran9@gmail.com>
2018-07-30 17:38:10 -0700

    Modifying class to run the development profile.

--- src/test/java/tran/example/weatherforecast/controllers/ForecastControllerIntegrationTest.java
+++ src/test/java/tran/example/weatherforecast/controllers/ForecastControllerIntegrationTest.java
@@ -5,16 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
-
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -34,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles({"development"})
 public class ForecastControllerIntegrationTest {
     /**
      * Entry point for MVC testing.
