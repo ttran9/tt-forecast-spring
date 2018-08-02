7a5bf3384027b0e95d4f2faaf9dc93af9423cf7c
todd <toddtran9@gmail.com>
2018-08-01 17:23:46 -0700

    Updating the documentation of the registerUser method.

--- src/main/java/tran/example/weatherforecast/services/registrationservices/RegistrationService.java
+++ src/main/java/tran/example/weatherforecast/services/registrationservices/RegistrationService.java
@@ -9,10 +9,12 @@ import tran.example.weatherforecast.domain.CustomUser;
 public interface RegistrationService {
 
     /**
-     * Takes in the userName and password from the form and saves it into the CustomUser object and
-     * then the password is encrypted and the user is saved into the database.
+     * Verifies that the user name is not taken and if not it takes in the userName and password
+     * from the form and saves it into the CustomUser object and then the password is encrypted
+     * and the user is saved into the database.
      * @param registrationFormCommand The object holding the user name and password.
-     * @return Returns a user object with a username and an encrypted password.
+     * @return Returns a user object with a username and an encrypted password, or null if the
+     * user does not exist.
      */
     CustomUser registerUser(RegistrationFormCommand registrationFormCommand);
 }
